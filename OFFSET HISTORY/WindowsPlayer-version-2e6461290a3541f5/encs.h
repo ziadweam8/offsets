8bb5a3139dfad0ffdcee07d02bf8d5324f72a724
ede121 <ziadweam82@gmail.com>
2026-04-23 14:15:59 +0200

    adding encs, adding offset history, updating readme

---
+++ OFFSET HISTORY/WindowsPlayer-version-2e6461290a3541f5/encs.h
@@ -0,0 +1,14 @@
+#define CLOSURE_CONT_ENC        VMValue1
+#define CLOSURE_DEBUGNAME_ENC   VMValue2
+#define LSTATE_STACKSIZE_ENC    VMValue4
+#define PROTO_ABSLINEINFO_ENC   VMValue1
+#define PROTO_DEBUGINSN_ENC     VMValue2
+#define PROTO_DEBUGNAME_ENC     VMValue3
+#define PROTO_LINEINFO_ENC      VMValue4
+#define PROTO_LOCVARS_ENC       VMValue2
+#define PROTO_SOURCE_ENC        VMValue1
+#define PROTO_TYPEINFO_ENC      VMValue3
+#define PROTO_UPVALUES_ENC      VMValue4
+#define PROTO_USERDATA_ENC      VMValue3
+#define TSTRING_HASH_ENC        VMValue3
+#define UDATA_META_ENC          VMValue3
