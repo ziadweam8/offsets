6fad626fef24ea9ce63202e52b30ef5d3174946b
ziadweam8 <113268204+ziadweam8@users.noreply.github.com>
2026-05-04 07:28:08 +0300

    Update in latestencs.h

--- latestencs.h
+++ latestencs.h
@@ -1,19 +1,20 @@
 -- dumped by ziadweam8
 
 #pragma once
-// version-390ba09e7e944154
-// may be incorrect
-#define CLOSURE_CONT_ENC               VMValue1
-#define CLOSURE_DEBUGNAME_ENC          VMValue2
-#define LSTATE_STACKSIZE_ENC           VMValue4
-#define PROTO_ABSLINEINFO_ENC          VMValue4
-#define PROTO_DEBUGINSN_ENC            VMValue4
-#define PROTO_DEBUGNAME_ENC            VMValue2
-#define PROTO_LINEINFO_ENC             VMValue4
-#define PROTO_LOCVARS_ENC              VMValue2
-#define PROTO_SOURCE_ENC               VMValue1
-#define PROTO_TYPEINFO_ENC             VMValue3
-#define PROTO_UPVALUES_ENC             VMValue4
-#define PROTO_USERDATA_ENC             VMValue3
-#define TSTRING_HASH_ENC               VMValue3
-#define UDATA_META_ENC                 VMValue3
+// version-acc4...
+
+
+#define PROTO_LINEINFO_ENC       VMValue4 
+#define PROTO_LOCVAR_ENC         VMValue1 
+#define PROTO_UPVALUES_ENC       VMValue2  
+#define PROTO_ABSLINEINFO_ENC    VMValue1  
+#define PROTO_SOURCE_ENC         VMValue3  
+#define PROTO_DEBUGISN_ENC       VMValue2 
+#define PROTO_DEBUGNAME_ENC      VMValue1 
+#define PROTO_TYPEINFO_ENC       VMValue2 
+#define PROTO_USERDATA_ENC       VMValue3 
+#define TSTRING_HASH_ENC         VMValue4  
+#define LSTATE_STACKSIZE_ENC     VMValue4  
+#define CLOSURE_CONT_ENC         VMValue3
+#define CLOSURE_DEBUGNAME_ENC    VMValue4
+#define UDATA_META_ENC           VMValue4
