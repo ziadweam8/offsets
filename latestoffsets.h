5da1e29c8f053675bf75cb0e37fa80ecabeb0ee5
ziadweam8 <113268204+ziadweam8@users.noreply.github.com>
2026-05-05 23:33:36 +0300

    more offsets

--- latestoffsets.h
+++ latestoffsets.h
@@ -4,9 +4,9 @@
   Zela Dumper 
    Made by king_okr & ziadweam8
    rbx LIVE-WindowsPlayer-version-acc4b74f79e743b9
-   Time taken 3.72s
-   Offsets found 22
-   Date 2026-04-30 20:20:11
+   Time taken 4.73s
+   Offsets found 22 -> 28
+   Date 2026-05-05 15:07:02
 
 */
 
@@ -23,7 +23,7 @@ namespace Offsets
 {
     const uintptr_t Print = REBASE(0x1E21AD0);
     const uintptr_t OpcodeLookupTable = REBASE(0x646C630);
-    const uintptr_t ScriptContextResume = REBASE(0x1D7DCD0);
+    const uintptr_t ScriptContextResume = REBASE(0x1D7CE90);
     const uintptr_t GetLuaStateForInstance = REBASE(0x1C4BAD0);
 
     namespace Luau
@@ -31,40 +31,58 @@ namespace Offsets
         const uintptr_t Luau_Execute = REBASE(0x43AD490);
         const uintptr_t LuaO_NilObject = REBASE(0x5F52318);
         const uintptr_t LuaH_DummyNode = REBASE(0x5F518C8);
-        const uintptr_t LuaD_throw = REBASE(0x4393840);
     }
 
     namespace DataModel
     {
         const uintptr_t ChildrenStart = 0x78;
         const uintptr_t GameLoaded = 0x5F8;
+        // ScriptContext Not found
     }
 
     namespace ExtraSpace
     {
         const uintptr_t RequireBypass = REBASE(0x928);
-        const uintptr_t IsCoreScript = REBASE(0x160); // guessed, may be incorrect
+        const uintptr_t IsCoreScript = REBASE(0x160);
+        // ScriptContextToResume Not found
     }
 
     namespace Globals
     {
+        // luaT_typenames Not found
         const uintptr_t WebSocketServiceEnableClientCreation = REBASE(0x63925D0);
+        // luaT_Eventnames Not found
         const uintptr_t KTable = REBASE(0x751A7B0);
+        // FlogDataBank Not found
     }
 
     namespace TaskScheduler
     {
         const uintptr_t RawScheduler = REBASE(0x7CF5400);
         const uintptr_t TaskSchedulerTargetFps = 0x118;
+        const uintptr_t TaskSchedulerPointer = REBASE(0x7CF5400);
+        const uintptr_t TaskSchedulerJobsStart = 0xC8;
+        const uintptr_t TaskSchedulerJobName = 0x18;
+    }
+
+    namespace Raknet
+    {
+        const uintptr_t ProcessNetworkPacket = REBASE(0x134A750);
+        // HandleConnectionState Not found
+        // ReportNetworkError Not found
     }
 
     namespace Misc
     {
         const uintptr_t FireTouchInterest = REBASE(0x83F430);
+        // fireProximityPrompt Not found
         const uintptr_t pushInstance = REBASE(0x1D43290);
         const uintptr_t luaf_newproto = REBASE(0x43FE8C0);
+        const uintptr_t GetCurrentThreadId = REBASE(0x2F40);
         const uintptr_t IdentityPtr = REBASE(0x7661528);
+        const uintptr_t GetIdentityStruct = REBASE(0x9060);
         const uintptr_t EnableLoadModule = REBASE(0x75A0878);
+        // HashTableLookup Not found
         const uintptr_t LockViolationInstanceCrash = REBASE(0x7598008);
     }
 }
